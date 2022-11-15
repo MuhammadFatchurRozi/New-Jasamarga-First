49299c0762c13937f95c517dcbd6cf3124b66f54
FatchurRozi <91874081+MuhammadFatchurRozi@users.noreply.github.com>
2022-11-15 11:19:57 +0700

    update navbar 15-11-2022 11:19

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,36 +1,15 @@
 (function($) {
-
     "use strict";
 
     // Page loading animation
-    $(window).on('load', function() {
-
-        $('#js-preloader').addClass('loaded');
-
+    $(window).on("load", function() {
+        $("#js-preloader").addClass("loaded");
     });
-
-    //client section owl carousel
-    // $(".owl-carousel").owlCarousel({
-    //     loop: true,
-    //     margin: 10,
-    //     nav: true,
-    //     dots: false,
-    //     navText: [
-    //         '<i class="fa fa-long-arrow-left" aria-hidden="true"></i>',
-    //         '<i class="fa fa-long-arrow-right" aria-hidden="true"></i>'
-    //     ],
-    //     autoplay: true,
-    //     autoplayHoverPause: true,
-    //     responsive: {
-    //         0: {
-    //             items: 1
-    //         },
-    //         768: {
-    //             items: 2
-    //         },
-    //         1000: {
-    //             items: 2
-    //         }
-    //     }
-    // });
+    // Menu Dropdown Toggle
+    if ($(".menu-trigger").length) {
+        $(".menu-trigger").on("click", function() {
+            $(this).toggleClass("active");
+            $(".header-area .nav").slideToggle(200);
+        });
+    }
 })(window.jQuery);
